312f859cd37e8be11d8f5aaa6aa55fb325f563e9
tonyrong <tony@tony.local>
2016-10-28 23:36:36 -0300

    add develop branch

---
+++ express_demo/Server.js
@@ -0,0 +1,18 @@
+var express	= require("express");
+var app		= express();
+var path	= require("path");
+
+app.get('/', function(req,res){
+	res.sendFile(path.join(
+			__dirname+'/index.html'
+		));
+});
+
+app.get('/about',function(req,res){
+	res.sendFile(path.join(
+		__dirname+'/about.html'
+		));
+});
+
+app.listen(3000);
+console.log("running at port 3000");
